15db27cd74f4d37a1a0a94c5cb8657fee3687b58
Japs35 <japs25112011@gmail.com>
2024-09-03 22:33:17 -0500

    hola py paso1

---
+++ hola.py
@@ -0,0 +1,30 @@
+import json
+#import numpy as np
+#from tensorflow.keras.models import load_model
+
+# Cargar la estructura del modelo desde el archivo JSON
+#with open('/ruta/destino/modelo.json', 'r') as json_file:
+#    modelo_json = json_file.read()
+
+# Cargar el modelo
+#modelo = load_model('/ruta/destino/modelo.h5')
+
+# Función para predecir el precio del departamento
+#def predecir_precio(area, banos, cuartos):
+    # Crear un array con los valores de entrada
+ #   entrada = np.array([[area, banos, cuartos]])
+    # Hacer la predicción
+  #  precio = modelo.predict(entrada)
+   # return precio[0][0]
+
+# Solicitar datos al usuario
+#area = float(input("Introduce el área del departamento (m²): "))
+#banos = int(input("Introduce el número de baños: "))
+#cuartos = int(input("Introduce el número de cuartos: "))
+
+# Hacer la predicción
+#precio_estimado = predecir_precio(area, banos, cuartos)
+
+# Mostrar el resultado
+#print(f"El precio estimado del departamento es: ${precio_estimado:.2f}")
+print(f"El precio estimado del departamento es: ")
