e6a7b2d1b838a2bb9c296944f608bc846b56079f
Japs35 <japs25112011@gmail.com>
2024-09-03 23:30:44 -0500

    primer cdigo funcional

--- hola.py
+++ hola.py
@@ -1,30 +1,38 @@
 import json
-#import numpy as np
-#from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.models import model_from_json
 
 # Cargar la estructura del modelo desde el archivo JSON
-#with open('/ruta/destino/modelo.json', 'r') as json_file:
-#    modelo_json = json_file.read()
+with open('notas.json', 'r') as json_file:
+    modelo_json = json_file.read()
 
-# Cargar el modelo
-#modelo = load_model('/ruta/destino/modelo.h5')
+# Reconstruir el modelo a partir de la configuración JSON
+modelo = model_from_json(modelo_json)
+
+# Compilar el modelo (necesario si vas a usar el modelo para predicción)
+modelo.compile(optimizer='adam', loss='mse')  # Ajusta el optimizador y la función de pérdida según tu modelo original
+
 
 # Función para predecir el precio del departamento
-#def predecir_precio(area, banos, cuartos):
+def predecir_carrera(MATEMATICAS, LENGUAJE, CIENCIASNATURALES, FISICA, CIENCIASSOCIALES):
     # Crear un array con los valores de entrada
- #   entrada = np.array([[area, banos, cuartos]])
+    entrada = np.array([[MATEMATICAS, LENGUAJE, CIENCIASNATURALES, FISICA, CIENCIASSOCIALES]])
     # Hacer la predicción
-  #  precio = modelo.predict(entrada)
-   # return precio[0][0]
+    carrera = modelo.predict(entrada)
+    return carrera[0][0]
 
 # Solicitar datos al usuario
-#area = float(input("Introduce el área del departamento (m²): "))
-#banos = int(input("Introduce el número de baños: "))
-#cuartos = int(input("Introduce el número de cuartos: "))
+MATE = int(input("Introduce la nota de matemática: "))
+LEN = int(input("Introduce la nota de lenguaje: "))
+CIENNAT = int(input("Introduce la nota de ciencias naturales: "))
+FIS = int(input("Introduce la nota de física: "))
+CIENSOC = int(input("Introduce la nota de ciencias sociales: "))
+
 
 # Hacer la predicción
-#precio_estimado = predecir_precio(area, banos, cuartos)
+carrera_estimada = predecir_carrera(MATE,LEN,CIENNAT,FIS,CIENSOC)
 
-# Mostrar el resultado
-#print(f"El precio estimado del departamento es: ${precio_estimado:.2f}")
-print(f"El precio estimado del departamento es: ")
+if carrera_estimada>=0.5:
+  print("Estudiaría Ingenieria")
+else:
+  print("Estudiaría Economía")
